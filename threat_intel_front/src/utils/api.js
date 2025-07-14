b7350bdad82caaf2fbfdd09c35a1578ac13ca851
lixingyu1 <lixingyu1@tinman.cn>
2025-07-14 17:22:17 +0800

    port

--- threat_intel_front/src/utils/api.js
+++ threat_intel_front/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 // 创建axios实例
 const api = axios.create({
-  baseURL: import.meta.env.VITE_APP_API_BASE_URL || 'http://localhost:5001/api',
+  baseURL: import.meta.env.VITE_APP_API_BASE_URL || 'http://localhost:8891/api',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json'
