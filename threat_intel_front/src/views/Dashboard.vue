96505dc09dd97a972e4a38e500fe9e0bc95eac5c
lixingyu1 <lixingyu1@tinman.cn>
2025-08-14 11:05:24 +0800

    fix

--- threat_intel_front/src/views/Dashboard.vue
+++ threat_intel_front/src/views/Dashboard.vue
@@ -294,7 +294,7 @@ export default {
 .content-grid {
   /* 确保始终为三列布局 */
   display: grid;
-  grid-template-columns: 0.7fr 1.2fr 0.7fr;
+  grid-template-columns: 0.7fr 1.0fr 0.7fr;
   gap: 2rem;
   min-height: 600px;
 }
